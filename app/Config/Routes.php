0e1625d22c2ffd6b2e0c16163973dd42eefb6d3f
Omar Pachas <jesuspachas0403@gmail.com>
2025-08-23 21:23:46 -0500

    Controlador paciente agregado

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+//Rutas pacientes
+$routes->get('/pacientes', 'PacienteController::index');
